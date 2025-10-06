ff0901354550bfb2da517c6bffb2e2868e4a68ab
khushbu465 <khushbukm133@gmail.com>
2025-10-06 17:50:03 +0530

    updated

--- src/Page/FormCom.jsx
+++ src/Page/FormCom.jsx
@@ -1,6 +1,8 @@
 import { Row, Col, Form, FormGroup, Button, Input, Label, FormFeedback, Modal, ModalHeader, ModalBody, Spinner } from 'reactstrap'
 import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-toastify';
+import { GetCity, GetCountries, GetState } from 'react-country-state-city';
+import Select from 'react-select';
 
 
 const FormCom = () => {
@@ -10,10 +12,27 @@ const FormCom = () => {
         tentative_date: '',
         institute: '', department: ''
     });
+    const [selectedState, setSelectedState] = useState("");
+    const [stateList, setStateList] = useState([]);
+    const [error, setError] = useState("");
+
     const [dateError, setDateError] = useState();
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        loadStates();
+    }, []);
 
+    //load states
+    const loadStates = async () => {
+        const stateList = await GetState(101);
+        const formattedStates = stateList.map((state) => ({
+            label: state.name,
+            value: state.id,
+            ...state,
+        }));
+        setStateList(formattedStates);
+    };
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (e.target.checkValidity()) {
@@ -53,6 +72,9 @@ const FormCom = () => {
             if (!formData.tentative_date) {
                 setDateError('Date of birth is required')
             }
+            if (!selectedState) {
+                setError('State is required')
+            }
         }
     };
     const handleChange = (e) => {
@@ -65,21 +87,32 @@ const FormCom = () => {
                 <Row>
                     <Col lg={6} md={12} >
                         <FormGroup>
+                            <Label> Product <span className='text-danger'>*</span></Label>
+                            <Input type="select" name="product_name" value={formData.product_name} onChange={handleChange} required  >
+                                <option value="">Select Product</option>
+                                <option value="University Management System">University Management System</option>
+                                <option value="Learning Management System">Learning Management System</option>
+                                <option value="Game Parlor Management System">Game Parlor Management System</option>
+                                <option value="Clinic Management System">Clinic Management System</option>
+                                <option value="Salon Management System">Salon Management System</option>
+                                <option value="Cold Storage Software">Cold Storage Software</option>
+                                <option value="Inventory Management System">Inventory Management System</option>
+                            </Input>
+
+                            <FormFeedback>This field is required</FormFeedback>
+                        </FormGroup>
+                    </Col>
+                    <Col lg={6} md={12} >
+                        <FormGroup>
                             <Label> Client Name  <span className='text-danger'>*</span></Label>
                             <Input type="text" name="client_name" value={formData.client_name} onChange={handleChange} placeholder="Enter Client Name" required />
                             <FormFeedback>This field is required</FormFeedback>
                         </FormGroup>
                     </Col>
+
                     <Col lg={6} md={12} >
                         <FormGroup>
-                            <Label> Product <span className='text-danger'>*</span></Label>
-                            <Input type="text" name="product_name" value={formData.product_name} onChange={handleChange} placeholder="Enter Product Name" required />
-                            <FormFeedback>This field is required</FormFeedback>
-                        </FormGroup>
-                    </Col>
-                    <Col lg={6} md={12} >
-                        <FormGroup>
-                            <Label for="dob"> Tentative  Date <span className='text-danger'>*</span></Label>
+                            <Label for="dob"> Tentative  Date of Project Starting <span className='text-danger'>*</span></Label>
                             <Input type="date"
                                 className={`form-control ${dateError ? 'is-invalid' : ''}`} placeholder=" Tentative Date" required
                                 value={formData.tentative_date} name="tentative_date"
@@ -90,16 +123,28 @@ const FormCom = () => {
                     </Col>
                     <Col lg={6} md={12} >
                         <FormGroup>
-                            <Label> Institute <span className='text-danger'>*</span></Label>
-                            <Input type="text" name="institute" value={formData.institute} onChange={handleChange} placeholder="Enter Institute Name" required />
+                            <Label> Institute/Department <span className='text-danger'>*</span></Label>
+                            <Input type="text" name="institute" value={formData.institute} onChange={handleChange} placeholder="Enter Institute/Department Name" required />
                             <FormFeedback>This field is required</FormFeedback>
                         </FormGroup>
                     </Col>
                     <Col lg={6} md={12} >
                         <FormGroup>
-                            <Label> Department <span className='text-danger'>*</span></Label>
-                            <Input type="text" name="department" value={formData.department} onChange={handleChange} placeholder="Enter Department Name" required />
-                            <FormFeedback>This field is required</FormFeedback>
+                            <Label> State <span className='text-danger'>*</span></Label>
+                            <Select
+                                value={selectedState ? selectedState : ''}
+                                className={error ? "its_require" : ''}
+                                name="state_province"
+                                id="state"
+                                options={stateList}
+                                onChange={(selectedOption) => {
+                                    setSelectedState(selectedOption);
+                                    setError('');
+                                }}
+                                placeholder="Select State"
+                                isSearchable={true} required
+                            />
+                            <small className='text-danger'>{error ? error : ''}</small>
                         </FormGroup>
                     </Col>
 
--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -8,16 +8,12 @@ const HomePage = () => {
       <div className='dashboard_page py-5'>
         <Container >
           <Row className='d-flex justify-content-center'>
-            <Col md={10}>
+            <Col md={10} lg={10} sm={12} className='col-12'>
               <Card>
                 <CardBody>
-                  {/* <CardImg src='https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1LzktMjYtdC0xMzU5MS5qcGc.jpg' alt='top_img' /> */}
                   <div class="topBgImgHere">
-                    <div class="lightLayer">
-                    <h2>Business Management <br/> System</h2>
                   </div>
-                  </div>
-                  <div class="form_starthere py-lg-5 px-5">
+                  <div class="form_starthere pt-3 py-lg-5 px-5">
                     <FormCom />
                   </div>
                 </CardBody>
